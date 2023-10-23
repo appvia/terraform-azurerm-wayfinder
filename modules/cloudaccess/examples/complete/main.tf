d9831dbc43e927bad9f91cd964fab2cb91bb6d2d
Mark Hughes <mrsheepuk@users.noreply.github.com>
2023-10-23 10:44:47 +0100

    Add complete example for cloudaccess submodule, add new required permissions to cluster manager and DNS zone manager (#10)
    * Add complete example for using cloudaccess module that can directly be used with a init -from-module + tfvars
    
    * Remove individual examples in favour of one with multiple tfvars examples
    
    * Correct DNS zone manager permission
    
    * Correct cluster manager permission

---
+++ modules/cloudaccess/examples/complete/main.tf
@@ -0,0 +1,19 @@
+module "wayfinder_cloudaccess" {
+  source = "github.com/appvia/terraform-azurerm-wayfinder//modules/cloudaccess"
+
+  resource_suffix = var.resource_suffix
+  region          = var.region
+
+  wayfinder_identity_azure_principal_id     = var.wayfinder_identity_azure_principal_id
+  wayfinder_identity_azure_tenant_id        = var.wayfinder_identity_azure_tenant_id
+  wayfinder_identity_aws_role_arn           = var.wayfinder_identity_aws_role_arn
+  wayfinder_identity_aws_issuer             = var.wayfinder_identity_aws_issuer
+  wayfinder_identity_aws_subject            = var.wayfinder_identity_aws_subject
+  wayfinder_identity_gcp_service_account    = var.wayfinder_identity_gcp_service_account
+  wayfinder_identity_gcp_service_account_id = var.wayfinder_identity_gcp_service_account_id
+
+  enable_cluster_manager  = var.enable_cluster_manager
+  enable_dns_zone_manager = var.enable_dns_zone_manager
+  enable_network_manager  = var.enable_network_manager
+  enable_cloud_info       = var.enable_cloud_info
+}
---
+++ modules/cloudaccess/examples/complete/outputs.tf
@@ -0,0 +1,6 @@
+output "managed_identity_client_id" {
+  value = module.wayfinder_cloudaccess.managed_identity_client_id
+}
+output "managed_identity_principal_id" {
+  value = module.wayfinder_cloudaccess.managed_identity_principal_id
+}
