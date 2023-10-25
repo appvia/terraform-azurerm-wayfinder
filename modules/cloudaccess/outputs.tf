8dc490647d2c961636a23bcffa6d84f2bb28ac6f
Kashif Saadat <kashifsaadat@gmail.com>
2023-10-25 16:19:29 +0100

    Remove unnecessary variables from cloudaccess submodule (#16)
    * Repo rename
    
    * Update CI workflow to scan submodules
    
    * Remove unnecessary variables from cloudaccess module
    
    * Fix issue with output when no federated identity is created.

--- modules/cloudaccess/outputs.tf
+++ modules/cloudaccess/outputs.tf
@@ -1,9 +1,9 @@
 output "managed_identity_client_id" {
   description = "The client ID of the created managed identity to use as spec.azure.clientID in your cloud access configuration"
-  value       = join("", azurerm_user_assigned_identity.federated_identity.*.client_id)
+  value       = var.from_aws || var.from_gcp ? azurerm_user_assigned_identity.federated_identity[0].client_id : ""
 }
 
 output "managed_identity_principal_id" {
   description = "The service principal ID of the created managed identity to use as spec.azure.principalID in your cloud access configuration"
-  value       = join("", azurerm_user_assigned_identity.federated_identity.*.principal_id)
+  value       = var.from_aws || var.from_gcp ? azurerm_user_assigned_identity.federated_identity[0].principal_id : ""
 }
