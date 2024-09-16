1bf951659c425d4bc90b50debcf61b605227f31b
Kashif Saadat <kashifsaadat@gmail.com>
2024-09-16 12:06:11 +0100

    refactor: switch to supported kubectl provider

--- versions.tf
+++ versions.tf
@@ -15,8 +15,8 @@ terraform {
       version = ">= 2.9.0"
     }
     kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
+      source  = "alekc/kubectl"
+      version = ">= 2.0"
     }
     time = {
       source  = "hashicorp/time"
