3c6a8b3606f5d6cf599ab71a8ab70ffc2b73b3ac
Adam Smith <alice@Adams-MacBook-Pro.local>
2024-10-11 12:33:12 +0100

    chore: set role definitions ids instead of names

--- modules/cloudaccess/wf_network_manager.tf
+++ modules/cloudaccess/wf_network_manager.tf
@@ -30,9 +30,9 @@ resource "time_sleep" "after_azurerm_role_definition_networkmanager" {
 resource "azurerm_role_assignment" "networkmanager" {
   count = var.enable_network_manager && var.from_azure ? 1 : 0
 
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = azurerm_role_definition.networkmanager[0].name
-  principal_id         = var.wayfinder_identity_azure_principal_id
+  scope              = data.azurerm_subscription.primary.id
+  role_definition_id = azurerm_role_definition.networkmanager[0].role_definition_resource_id
+  principal_id       = var.wayfinder_identity_azure_principal_id
 
   depends_on = [
     time_sleep.after_azurerm_role_definition_networkmanager[0],
@@ -43,9 +43,9 @@ resource "azurerm_role_assignment" "networkmanager" {
 resource "azurerm_role_assignment" "networkmanager_federated" {
   count = var.enable_network_manager && (var.from_aws || var.from_gcp) ? 1 : 0
 
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = azurerm_role_definition.networkmanager[0].name
-  principal_id         = azurerm_user_assigned_identity.federated_identity[0].principal_id
+  scope              = data.azurerm_subscription.primary.id
+  role_definition_id = azurerm_role_definition.networkmanager[0].role_definition_resource_id
+  principal_id       = azurerm_user_assigned_identity.federated_identity[0].principal_id
 
   depends_on = [
     time_sleep.after_azurerm_role_definition_networkmanager[0],
