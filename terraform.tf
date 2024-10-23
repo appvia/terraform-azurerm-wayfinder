a9c43d576feda7b15d8db31f69028ae0cc22c5ea
Rohith Jayawardene <gambol99@gmail.com>
2024-10-23 17:23:22 +0100

    chore: changing the name of the file for consistency with other modules

---
+++ terraform.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.53.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.51.0, < 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9.0"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5"
+    }
+  }
+}
