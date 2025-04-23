7a56d8527c3ee9c8ed9599cd21936579257b86bb
Lewis Marshall <lewis.marshall@appvia.io>
2025-04-23 15:46:21 +0100

    feat: make v3 a thing

--- modules/cloudaccess/examples/complete/main.tf
+++ modules/cloudaccess/examples/complete/main.tf
@@ -1,5 +1,5 @@
 module "wayfinder_cloudaccess" {
-  source = "github.com/appvia/terraform-azurerm-wayfinder//modules/cloudaccess"
+  source = "github.com/appvia/terraform-azurerm-wayfinder//modules/cloudaccess?ref=v3"
 
   resource_suffix = var.resource_suffix
   region          = var.region
