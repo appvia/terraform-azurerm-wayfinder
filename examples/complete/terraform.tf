01579f95b67fb0e2abaf9f30a744ab7e50f52e17
Lewis Marshall <lewismarshall@users.noreply.github.com>
2025-04-24 09:31:01 +0100

    add parameter for subscription id for configuring provider (#96)

--- examples/complete/terraform.tf
+++ examples/complete/terraform.tf
@@ -30,5 +30,9 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.5"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
--- modules/cloudaccess/examples/complete/provider.tf
+++ modules/cloudaccess/examples/complete/provider.tf
@@ -1,4 +1,11 @@
 provider "azurerm" {
+  subscription_id            = var.subscription_id
   skip_provider_registration = true
   features {}
+}
+
+variable "subscription_id" {
+  default     = ""
+  description = "The subscription ID to apply role definitions in"
+  type        = string
 }
